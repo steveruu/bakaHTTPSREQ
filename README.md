017e1d3c6f3724e5b9ff8e3cfc1fd6d78fe0d47d
Štěpán Prchal <prchalek@gmail.com>
2023-03-27 13:38:31 +0200

    zabijsu se

---
+++ README.md
@@ -0,0 +1,2 @@
+# baka http requests
+* chytam merva
--- index.js
+++ index.js
@@ -17,19 +17,19 @@ axios.post(`${bakaURL}/api/login`, data, {
   accessToken = response.data.access_token;
 })
 .catch(error => {
-  console.log(error.response.status, error.response.data.error_description);
+  console.log(error.response.status, error.response.data);
 });
 
 axios.get(`${bakaURL}/api/3/timetable/permanent`, data, {
     headers: {
+      'Authorization': `Bearer ${accessToken}`,
       'Content-Type': 'application/x-www-form-urlencoded'
     }
   })
   .then(response => {
     console.log(response.data);
-    accessToken = response.data.access_token;
   })
   .catch(error => {
-    console.log(error.response.status, error.response.data.error_description);
+    console.log(error.response.status, error.response.data);
   });
   
